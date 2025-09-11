ae9e475a09cb81bbc6f672086557463db78edf73
Aditya Mishra <mishraaaditya29@gmail.com>
2025-09-11 12:54:30 +0530

    added functionality to reverse arrays and vectors, and find maximum element in array

--- Data-Structure/0-Background/Striver's-Sheet/2-sort.cpp
+++ Data-Structure/0-Background/Striver's-Sheet/2-sort.cpp
@@ -23,4 +23,20 @@ int main()
     // vector -> {1,6,2,7,4} and we want to sort only index from 1 to 3
     // final vector -> {1,2,6,7,4}
     sort(v.begin() + 1, v.begin() + 4); // sorts the vector from index 1 to 3 [)
+
+    // reversing the array and vector
+    reverse(arr, arr + n);       // reverses the array
+    reverse(v.begin(), v.end()); // reverses the vector
+
+    // Finding max and min element in array and vector
+    // for any index i to j give the maximum
+    int maxi = INT_MIN;
+    // Example: find max in arr from index 0 to n-1
+    for (int k = 0; k < n; k++)
+    {
+        if (arr[k] > maxi)
+        {                  // compare with current maximum
+            maxi = arr[k]; // update maximum if current element is greater
+        }
+    }
 }
