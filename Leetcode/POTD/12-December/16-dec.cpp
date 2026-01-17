a04abf16385600f8018a2b338f412c8cea8eace0
Aditya Mishra <mishraaaditya29@gmail.com>
2026-01-17 16:16:48 +0530

    added implementation for Leetcode problem 3562: Maximum Profit from Trading Stocks with Discounts

---
+++ Leetcode/POTD/12-December/16-dec.cpp
@@ -0,0 +1,70 @@
+// leetcode 3562. Maximum Profit from Trading Stocks with Discounts
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> adj;
+    pair<vector<int>, vector<int>> dp[161];
+    bool solved[161];
+    vector<int> merge(vector<int> &a, vector<int> &b)
+    {
+        int bud = a.size();
+        vector<int> res(bud, -1e9);
+        for (int i = 0; i < bud; i++)
+        {
+            if (a[i] < 0)
+                continue;
+            for (int j = 0; i + j < bud; j++)
+                res[i + j] = max(res[i + j], a[i] + b[j]);
+        }
+        return res;
+    }
+    pair<vector<int>, vector<int>> solve(int node, vector<int> &present, vector<int> &future, int b)
+    {
+        if (solved[node])
+            return dp[node];
+        solved[node] = true;
+        vector<int> dp0(b + 1, 0);
+        vector<int> dp1(b + 1, 0);
+        for (int c : adj[node])
+        {
+            auto [c0, c1] = solve(c, present, future, b);
+            dp0 = merge(dp0, c0);
+            dp1 = merge(dp1, c1);
+        }
+        vector<int> res0 = dp0;
+        vector<int> res1 = dp0;
+        int full = present[node], half = present[node] / 2;
+        int profitFull = future[node] - full, profitHalf = future[node] - half;
+        for (int i = full; i <= b; i++)
+            res0[i] = max(res0[i], dp1[i - full] + profitFull);
+        for (int i = half; i <= b; i++)
+            res1[i] = max(res1[i], dp1[i - half] + profitHalf);
+        return dp[node] = {res0, res1};
+    }
+    int maxProfit(int n, vector<int> &present, vector<int> &future, vector<vector<int>> &hierarchy, int budget)
+    {
+        adj.assign(n, {});
+        memset(solved, false, sizeof(solved));
+        for (auto &e : hierarchy)
+            adj[e[0] - 1].push_back(e[1] - 1);
+        auto res = solve(0, present, future, budget);
+        return *max_element(res.first.begin(), res.first.end());
+    }
+};
+int main()
+{
+    Solution sol;
+    int n = 5;
+    vector<int> present = {100, 200, 300, 400, 500};
+    vector<int> future = {150, 250, 350, 450, 600};
+    vector<vector<int>> hierarchy = {{1, 2}, {1, 3}, {2, 4}, {2, 5}};
+    int budget = 700;
+    int result = sol.maxProfit(n, present, future, hierarchy, budget);
+    cout << "Maximum profit: " << result << endl;
+    return 0;
+}
