081949dc57bd12709271f730fcd2c223dc32c7b0
Aditya Mishra <mishraaaditya29@gmail.com>
2026-01-28 19:35:37 +0530

    added implementation for Leetcode problem 712: Minimum ASCII Delete Sum for Two Strings

---
+++ Leetcode/POTD/2026/1-Jan/10-jan.cpp
@@ -0,0 +1,43 @@
+// leetcode 712. Minimum ASCII Delete Sum for Two Strings
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+    int minimumDeleteSum(string s1, string s2)
+    {
+        int n = s1.length();
+        int m = s2.length();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i][0] = dp[i - 1][0] + (int)s1[i - 1];
+        }
+        for (int j = 1; j <= m; j++)
+        {
+            dp[0][j] = dp[0][j - 1] + (int)s2[j - 1];
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = min(dp[i - 1][j] + (int)s1[i - 1], dp[i][j - 1] + (int)s2[j - 1]);
+            }
+        }
+        return dp[n][m];
+    }
+};
+int main()
+{
+    Solution sol;
+    string s1 = "sea";
+    string s2 = "eat";
+    int result = sol.minimumDeleteSum(s1, s2);
+    return 0;
+}
